2d299835db8ea15b3b0549153ec7a7053408f911
FlDanyT <96938294+FlDanyT@users.noreply.github.com>
2023-10-28 17:39:39 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Header from "./components/header/header.js";
+import Main from "./components/main/main.js";
+import Pricing from './components/pricing/pricing.js'
+import Footer from './components/footer/footer.js'
+import { useRef } from "react";
+
+function App() {
+  const pageScrollMain = useRef(null); 
+  const scrollMain = () => {
+    pageScrollMain.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const pageScrollMainTwo = useRef(null); 
+  const scrollMainTwo = () => {
+    pageScrollMainTwo.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const pageScrollFooter = useRef(null); 
+  const scrollFooter = () => {
+    pageScrollFooter.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const pageScrollFooterTwo= useRef(null); 
+  const scrollFooterTwo= () => {
+    pageScrollFooterTwo.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div class="container">
+  <Header scrollMain={scrollMain} scrollMainTwo={scrollMainTwo} scrollFooter={scrollFooter} scrollFooterTwo={scrollFooterTwo}/>
+  <Main pageScrollMain={pageScrollMain} pageScrollMainTwo={pageScrollMainTwo}/>
+  {/* <Pricing/> */}
+<Footer pageScrollFooter={pageScrollFooter} pageScrollFooterTwo={pageScrollFooterTwo}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pricing/pricing.js
@@ -0,0 +1,30 @@
+import styles from './pricing.module.css'
+import cardFons from './img/Rectangle 60.png'
+
+function Pricing() {
+    return (
+      <div>
+   <section class="pricing row">
+            <div class="col-12">
+                <h1 className={styles.mainText}>Тарифы</h1>
+            </div>
+           </section>
+           <section class="pricingCard row">
+            <div class="col-3">
+                <img src={cardFons}/>
+                {/* <div class="block">Карточка товара №1</div> */}
+            </div>
+            <div class="col-6">
+            <img src={cardFons} className={styles.cardTwo} />
+                {/* <div class="block">Карточка товара №2</div> */}
+            </div>
+            <div class="col-3">
+            <img src={cardFons} className={styles.cardThree}/>
+            </div>
+           </section>
+      </div>
+    );
+  }
+  
+  export default Pricing;
+  
